bc134856087bfc1cac1755b2b9a86df1b8467f27
Harley Johnson <john8577@msu.edu>
2024-09-17 19:04:30 -0400

    Added other gift categories to character information. Removed comments and most print statements.

--- read-website-data.py
+++ read-website-data.py
@@ -10,10 +10,7 @@ soup = BeautifulSoup(req.content, "html.parser")
 #print(req.text)
 
 tables = soup.findAll("table")
-#tables = BeautifulSoup(req.content, "html.parser").table
-#print(tables)
 
-#determines if it should identify the table by an attribute or by containing specific text
 wantedTableInfos = [{
     "byAttribute": True,
     "attribute": "id",
@@ -23,6 +20,26 @@ wantedTableInfos = [{
     "byAttribute": False,
     "attribute": "",
     "value": "Universal Loves"
+},
+{
+    "byAttribute": False,
+    "attribute": "",
+    "value": "Universal Likes"
+},
+{
+    "byAttribute": False,
+    "attribute": "",
+    "value": "Universal Neutrals"
+},
+{
+    "byAttribute": False,
+    "attribute": "",
+    "value": "Universal Dislikes"
+},
+{
+    "byAttribute": False,
+    "attribute": "",
+    "value": "Universal Hates"
 }]
 
 def hasAttributeAs(item, attribute, value):
@@ -59,20 +76,12 @@ wantedTables = []
 count = 1
 for table in tables:
     tableInfo = getWantedTableInfo(table)
-    #print("Table Info", tableInfo)
     if tableInfo["isWanted"]:
-        #wantedTables.append(table)
         wantedTables.append({"table": table, "category": tableInfo["category"]})
-        print("Table", count, "is wanted!")
-        #print(table)
-        print("")
     count = count + 1
-
-print("Wanted tables count: ", len(wantedTableInfos), "Found: ", len(wantedTables))
 
 def isInfoTable(table):
     if hasAttributeAs(table, "id", "infoboxtable"):
-        print('Found infoboxtable!')
         return True
     return False
 
@@ -87,30 +96,23 @@ print("")
 
 characterInfo = []
 for tableWithCategory in wantedTables:
-    #print("tableWithCategory", tableWithCategory)
     table = tableWithCategory["table"]
     category = tableWithCategory["category"]
 
     rows = getTableRows(table)
-    #tableInfo = {"category": category, "information": []}
     if isInfoTable(table):
         count = 0
         for row in rows:
             cells = row.findAll("td")
             if count == 0:
-                #addCellToAttributeList(tableInfo["information"], "Name", cells[0].text.strip())
                 addToAttributeList(characterInfo, "Name", cells[0].text.strip())
-                print("Name: ",  cells[0].text)
             else:
                 valuesToKeep = ["Birthday", "Address", "Marriage"]
                 valueNameInRow = cells[0].text.strip()
                 if valueNameInRow in valuesToKeep:
                     newValue = cells[1].text.strip()
-                    #addCellToAttributeList(tableInfo["information"], valueNameInRow, newValue)
                     addToAttributeList(characterInfo, valueNameInRow, newValue)
-                    print("Found attribute", valueNameInRow, ":", newValue)
             count = count + 1
-        print("infotable values: ", tableInfo)
     else:
         rowCount = 0
         giftItems = []
@@ -124,37 +126,6 @@ for tableWithCategory in wantedTables:
                         break
                     cellCount = cellCount + 1
             rowCount = rowCount + 1
-        #     if count == 0:
-        #         addCellToAttributeList(tableInfo["information"], "Name", cells[0].text.strip())
-        #         print("Name: ",  cells[0].text)
-        #     else:
-        #         valuesToKeep = ["Birthday", "Address", "Marriage"]
-        #         valueNameInRow = cells[0].text.strip()
-        #         if valueNameInRow in valuesToKeep:
-        #             newValue = cells[1].text.strip()
-        #             addCellToAttributeList(tableInfo["information"], valueNameInRow, newValue)
-        #             print("Found attribute", valueNameInRow, ":", newValue)
-        #     count = count + 1
-        #print("infotable values: ", tableInfo)
-        #print("Gift Items: ", giftItems)
         addToAttributeList(characterInfo, category, giftItems)
 
-    #characterInfo.append(tableInfo)
-    
-        #print("rows for infoboxtable: ", rows)
 print("Character info: ", characterInfo)
-
-# count = 1
-# for table in tables:
-#     if count < 5:
-#         print("Table", count)
-#         # print(table)
-#         # print("")
-#         attributes = table.attrs
-#         print("Attributes: ", attributes)
-#         if ("id" in attributes):
-#             print("Table has id: ", attributes["id"])
-#             if hasAttributeAs(table, "id", "infoboxtable"):
-#                 print("Id is infoboxtable")
-#         print("")
-#     count = count + 1
