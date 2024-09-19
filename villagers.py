290c6fb56785c33d2e6e424c3a7bf3a86e91137a
Harley Johnson <john8577@msu.edu>
2024-09-19 15:07:02 -0400

    Added comment instructions for what to write.

---
+++ villagers.py
@@ -0,0 +1,9 @@
+# first grab all the character information from the file
+# turn the information into lists or objects
+
+# give options to look up an item or villager
+
+# when user looks up item, list which villagers love it, like it, are neutral, hate it, etc.
+# also count how many villagers like/hate the item
+
+# when user looks up villager name, show all the basic information about them
